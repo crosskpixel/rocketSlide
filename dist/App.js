86ca6703c27e9e3dd430c8a4a835b47b4a55ae98
Igor Pancheski <igorpancheskilinux@icloud.com>
2018-04-05 10:06:31 -0300

    correções e ativação do ServiceWorker

--- dist/App.js
+++ dist/App.js
@@ -37,12 +37,15 @@ class App {
             //  res.setHeader("Access-Control-Allow-Headers", "*");
             // res.setHeader('Access-Control-Allow-Credentials', "false");
             // res.setHeader('Access-Control-Max-Age', '1728000');
-            if (req.headers['x-forwarded-proto'] != 'https') {
-                res.redirect("https://" + req.headers.host + req.url);
+            if (process.env.NODE_ENV.trim() != "test") {
+                if (req.headers['x-forwarded-proto'] != 'https') {
+                    res.redirect("https://" + req.headers.host + req.url);
+                }
+                else {
+                    next();
+                }
             }
-            else {
-                next();
-            }
+            next();
         });
         this.express.use(express.static(path.join(__dirname, "public")));
     }
